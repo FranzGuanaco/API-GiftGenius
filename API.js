f03623da838d9fadb6b9bdc27236dff264d53a12
Pierre Chevin <chevin.pierre.tomas@gmail.com>
2024-02-05 23:52:50 +0100

    new architecture

--- API.js
+++ API.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Client } = require('pg');
 
 const app = express();
-const port = 3000; // Le port sur lequel votre API écoutera
+const port = 5432; // Le port sur lequel votre API écoutera
 
 // Configuration de la connexion à la base de données PostgreSQL
 const client = new Client({
@@ -18,20 +18,4 @@ client.connect()
   .then(() => console.log('Connecté à la base de données PostgreSQL avec succès'))
   .catch(err => console.error('Erreur lors de la connexion à la base de données PostgreSQL:', err));
 
-// Route pour récupérer tous les produits
-app.get('/api/products', async (req, res) => {
-  try {
-    const query = 'SELECT * FROM products';
-    const result = await client.query(query);
-    const products = result.rows;
-    res.json(products);
-  } catch (error) {
-    console.error('Une erreur s\'est produite lors de la récupération des produits:', error);
-    res.status(500).json({ error: 'Erreur serveur' });
-  }
-});
 
-// Écoute de l'API sur le port spécifié
-app.listen(port, () => {
-  console.log(`API en cours d'exécution sur le port ${port}`);
-});
